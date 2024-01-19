dcd149a230ada61b84b9168785659c16023d4016
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-19 23:47:29 +0800

    chore: add unit test

---
+++ deps/assert.ts
@@ -0,0 +1,2 @@
+// https://deno.land/x/lume@v2.0.0 dependencies
+export { assertEquals } from 'https://deno.land/std@0.208.0/assert/mod.ts'
---
+++ examples/lume-blog/_config.test.ts
@@ -0,0 +1,13 @@
+import site from './_config.ts'
+import { assertEquals } from '../../deps/assert.ts'
+
+// import { assertSiteSnapshot, build } from 'lume/tests/utils.ts'
+
+Deno.test('build examples/lume-blog', async () => {
+  // await build(site)
+  // await assertSiteSnapshot(t, site)
+  await site.build()
+    .catch(err => console.error(Deno.inspect(err, { colors: true })))
+
+  assertEquals(site.options.location, new URL('https://aoba-test.netlify.app'))
+})
