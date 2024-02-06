187401f060b566f190b91505d459c20b56a4d67b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-02-06 22:15:48 +0800

    feat: option to disable features

--- lume/plugins/hatsu.ts
+++ lume/plugins/hatsu.ts
@@ -15,63 +15,71 @@ export interface Options {
    * Match Routes.
    * @defaultValue `undefined`
    */
-  match?: RegExp[]
+  match?: RegExp[] | false
+  /**
+   * Whether to copy `/.well-known/` files.
+   * @defaultValue `undefined`
+   */
+  wellKnown?: false
 }
 
 export const hatsuPlugin = (options: Options): Plugin => (site: Lume.Site) => {
   // copy .well-known files
-  site.addEventListener('beforeRender', async ({ pages }) => {
-    pages.push(
-      // webfinger (with search params)
-      Page.create({
-        url: '/.well-known/webfinger',
-        content: await read(
-          new URL(
-            `/.well-known/webfinger?resource=acct:${site.options.location.host}@${options.instance.host}`,
-            options.instance,
-          ).href,
-          false,
-        ),
-      }),
-      // other
-      ...await Promise.all(
-        ['nodeinfo', 'host-meta', 'host-meta.json']
-          .map(async (file) =>
-            Page.create({
-              url: `/.well-known/${file}`,
-              content: await read(
-                new URL(`/.well-known/${file}`, options.instance).href,
-                false,
-              ),
-            })
+  if (options.wellKnown !== false) {
+    site.addEventListener('beforeRender', async ({ pages }) => {
+      pages.push(
+        // webfinger (with search params)
+        Page.create({
+          url: '/.well-known/webfinger',
+          content: await read(
+            new URL(
+              `/.well-known/webfinger?resource=acct:${site.options.location.host}@${options.instance.host}`,
+              options.instance,
+            ).href,
+            false,
           ),
-      ),
-    )
-  })
+        }),
+        // other
+        ...await Promise.all(
+          ['nodeinfo', 'host-meta', 'host-meta.json']
+            .map(async (file) =>
+              Page.create({
+                url: `/.well-known/${file}`,
+                content: await read(
+                  new URL(`/.well-known/${file}`, options.instance).href,
+                  false,
+                ),
+              })
+            ),
+        ),
+      )
+    })
+  }
 
   // set <link rel="alternate" type="application/activity+json" /> for matched page
-  site.process(['.html'], (pages) =>
-    pages.forEach((page) => {
-      // console.log(page.outputPath, page.data.url)
-      if (
-        page.document &&
-        (!options.match || options.match?.some((matcher) =>
-          (page.data.url as string).match(matcher)
-        ))
-      ) {
-        const link = page.document.createElement('link')
-        link?.setAttribute('rel', 'alternate')
-        link?.setAttribute('type', 'application/activity+json')
-        link?.setAttribute(
-          'href',
-          new URL(
-            `/posts/${new URL(page.data.url, site.options.location).href}`,
-            options.instance,
-          ).href,
-        )
-        page.document.head.appendChild(link)
-      }
-    }))
+  if (options.match) {
+    site.process(['.html'], (pages) =>
+      pages.forEach((page) => {
+        if (
+          page.document &&
+          ((options.match as RegExp[]).some((matcher) =>
+            (page.data.url as string).match(matcher)
+          ))
+        ) {
+          const link = page.document.createElement('link')
+          link?.setAttribute('rel', 'alternate')
+          link?.setAttribute('type', 'application/activity+json')
+          link?.setAttribute(
+            'href',
+            new URL(
+              `/posts/${new URL(page.data.url, site.options.location).href}`,
+              options.instance,
+            ).href,
+          )
+          page.document.head.appendChild(link)
+        }
+      }))
+  }
 }
 
 export default hatsuPlugin
